77ac25c8578860b8a9702bcc967085fd163f3010
Kallol Dey <kalloldey067@gmail.com>
2026-05-03 22:06:58 +0600

    bug fixed

--- src/app/(main)/books/[id]/page.jsx
+++ src/app/(main)/books/[id]/page.jsx
@@ -1,11 +1,70 @@
 
+// import BorrowButton from "@/components/BorrowButton";
+// import { getAllBooks } from "@/lib/data";
+// import Image from "next/image";
+
+// import { auth } from "@/lib/auth";
+// import { headers } from "next/headers";
+// import { redirect } from "next/navigation";
+
+
+
+
+// const BooksDetails = async ({ params }) => {
+//     const { id } = await params;
+//     const books =await getAllBooks();
+//     const { image_url, title, author, category, description, available_quantity } = books.find(b => b.id.toString() === id);
+
+
+//     const session = await auth.api.getSession({
+//             headers: await headers(),
+//         });
+
+        
+    
+//     return (
+// <div>
+//     {
+//         session?.user ? (
+//             <div className="flex p-5 bg-base-100 shadow-sm mx-5 md:mx-30 lg:mx-65 mt-20">
+//             <figure className="">
+//                 <Image  src={image_url} alt={title} height={250} width={250}></Image>
+//             </figure>
+//             <div className="card-body">
+//                 <h2 className="card-title">{title}</h2>
+//                 <p>Author: {author}</p>
+//                 <p>Type: {category}</p>
+//                 <p>Available: {available_quantity}</p>
+//                 <p>{description}</p>
+
+//                 <BorrowButton title={title}></BorrowButton>
+                
+//             </div>
+//         </div>
+//         ) : (
+//             redirect('/login')
+//         )
+//     }
+// </div>
+//     );
+// };
+
+// export default BooksDetails;
+
+
+        
+
+
+
+
+
+
+
+
 import BorrowButton from "@/components/BorrowButton";
 import { getAllBooks } from "@/lib/data";
 import Image from "next/image";
 
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 
 
@@ -15,17 +74,10 @@ const BooksDetails = async ({ params }) => {
     const books =await getAllBooks();
     const { image_url, title, author, category, description, available_quantity } = books.find(b => b.id.toString() === id);
 
-
-    const session = await auth.api.getSession({
-            headers: await headers(),
-        });
-
-        
+  
     
     return (
-<div>
-    {
-        session?.user ? (
+
             <div className="flex p-5 bg-base-100 shadow-sm mx-5 md:mx-30 lg:mx-65 mt-20">
             <figure className="">
                 <Image  src={image_url} alt={title} height={250} width={250}></Image>
@@ -41,11 +93,7 @@ const BooksDetails = async ({ params }) => {
                 
             </div>
         </div>
-        ) : (
-            redirect('/login')
-        )
-    }
-</div>
+        
     );
 };
 
