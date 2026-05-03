32be5df433f564a1e8de20a59e0e92b4613d11f6
Kallol Dey <kalloldey067@gmail.com>
2026-05-04 00:28:17 +0600

    add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://online-book-borrowing-platformm-wit.vercel.app"
+    baseURL: process.env.BETTER_AUTH_URL
 })
 
 export const { signIn, signUp, updateUser, useSession } = createAuthClient();
