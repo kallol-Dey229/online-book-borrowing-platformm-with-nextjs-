97c4c3b8ffa02ff6e19f1012a0b1650b465e1cf1
Kallol Dey <kalloldey067@gmail.com>
2026-05-03 21:58:35 +0600

    bug fixed

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,5 @@
 export const getAllBooks =async () =>{
     const res = await fetch("http://localhost:3000/books.json");
     const books = await res.json();
-    return books;
+    return books; 
 }
