208ba61d2ac0bbfd38b2fe672b74e0e58cd657e9
Kallol Dey <kalloldey067@gmail.com>
2026-05-04 11:20:39 +0600

    book details private route done

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,4 +2,4 @@ export const getAllBooks =async () =>{
     const res = await fetch("https://online-book-borrowing-platformm-wit.vercel.app/books.json");
     const books = await res.json();
     return books;
-}
+} 
